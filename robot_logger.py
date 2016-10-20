8106fe80c81cae86cd1939fe5d0efa0420bbf5ba
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2016-10-19 20:04:31 -0700

    cleaning up robot_logger

--- robot_logger.py
+++ robot_logger.py
@@ -7,17 +7,7 @@ def log(message):
 
 	cursor.execute("INSERT INTO RobotThoughtApp_log (description, reported, creation_time) VALUES (?, 0, ?)", (message, str(datetime.datetime.now())))
 
-	# for row in cursor.execute("SELECT * FROM RobotThoughtApp_log"):
-	# 	print row
-
 	conn.commit()
 	conn.close()
 
 	return
-
-# log('I am initializing the robot')
-# log('I am loading the objects')
-# log('I am opening the camera')
-# log('I am loading the grasps')
-# log('I am loading the object classifiers for registration')
-log('message')
