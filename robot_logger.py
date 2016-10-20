293539994f179b395894c7abfaf52a6f5e713ca6
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2016-10-19 17:03:59 -0700

    added DB full path name to robot_logger

--- robot_logger.py
+++ robot_logger.py
@@ -2,7 +2,7 @@ import sqlite3
 import datetime
 
 def log(message):
-	conn = sqlite3.connect('db.sqlite3')
+	conn = sqlite3.connect('/home/autolab/Workspace/rishi_working/echoyumi/db.sqlite3')
 	cursor = conn.cursor()
 
 	cursor.execute("INSERT INTO RobotThoughtApp_log (description, reported, creation_time) VALUES (?, 0, ?)", (message, str(datetime.datetime.now())))
