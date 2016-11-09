ac519870dbbc89839a12af3264bdef9d2134fd16
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2016-11-09 14:54:09 -0800

    adding dev and results mode

--- RobotThoughtApp/alexa.py
+++ RobotThoughtApp/alexa.py
@@ -11,6 +11,7 @@ from subprocess import call
 from RobotThoughtApp.models import Log
 
 
+
 @intent
 def GetRobotThought(session):
     """
@@ -26,6 +27,50 @@ def GetRobotThought(session):
     how are you doing
     speak
     continue
+    end stream
+    end audio
+    end audio stream
+    close stream
+    close audio
+    close audio stream
+    """
+    title, content = processAudio()
+    return ResponseBuilder.create_response(end_session=False,
+                                            title=title,
+                                            content=content
+                                          )
+
+
+@intent
+def RunInDevMode(session):
+    """
+    Default RunInDevMode Intent
+    ---
+    run in dev mode
+    run dev mode
+    run dev
+    dev mode
+    dev
+    """
+    global audio_thread
+    if audio_thread is not None:
+        stop_bluetooth_thread()
+    title, content = processAudio(play_music=False)
+    return ResponseBuilder.create_response(end_session=False,
+                                            title=title,
+                                            content=content
+                                          )
+
+
+@intent
+def RunInResultsMode(session):
+    """
+    Default RunInResultsMode Intent
+    ---
+    run in results mode
+    tell me the results
+    show me the results
+    results
     """
     global audio_thread
     if audio_thread is not None:
@@ -33,44 +78,61 @@ def GetRobotThought(session):
         title = "Stopping Audio Stream"
         content = "Stopping the Bluetooth audio stream."
     else:
-        start_bluetooth_thread()
-        title = "Play Audio Stream"
-        content = "Streaming the robot logs via Bluetooth."
-    return ResponseBuilder.create_response(end_session=True, # should be true for audio streams
+        title = "Results Mode"
+        content = "Reporting the results so far."
+    
+    engine = tts.init()
+    engine.setProperty('rate', 150)
+    engine.setProperty('voice', 'english-us')
+    engine.startLoop(False)
+
+    message = "Testing results mode." # TODO
+    engine.say(message)
+    engine.iterate()
+
+    engine.endLoop()
+    engine.stop()
+
+    return ResponseBuilder.create_response(end_session=False,
                                             title=title,
                                             content=content
                                           )
-    # return ResponseBuilder.create_response(message="",
-    #                                         reprompt="",
-    #                                         end_session=True, # should be true for audio streams
-    #                                         title="Play Audio Stream",
-    #                                         content="Streaming the robot logs.",
-    #                                         directives=[
-    #                                             ResponseBuilder.create_stream_directive(
-    #                                                 playBehavior="REPLACE_ALL",
-    #                                                 token="explainable-audio",
-    #                                                 url="https://134e60a0.ngrok.io/audio.mp3"
-    #                                             )
-    #                                         ]
-    #                                     )
+
+
+
+
+def processAudio(play_music=True):
+    global audio_thread
+    if audio_thread is not None:
+        stop_bluetooth_thread()
+        title = "Stopping Audio Stream"
+        content = "Stopping the Bluetooth audio stream."
+    else:
+        start_bluetooth_thread(play_music)
+        title = "Playing Audio Stream"
+        content = "Streaming the robot logs via Bluetooth."
+    return title, content
+
 
 
 
 class AudioThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, play_music=True):
         super(AudioThread, self).__init__()
         self._stop_event = threading.Event()
+        self.play_music = play_music
 
-        self.CHUNK = 1024
-        file_path = os.path.join(settings.STATIC_ROOT, 'song.wav')
-        self.wf = wave.open(file_path, 'rb')
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format = self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels = self.wf.getnchannels(),
-            rate = self.wf.getframerate(),
-            output = True
-        )
+        if self.play_music:
+            self.CHUNK = 1024
+            file_path = os.path.join(settings.STATIC_ROOT, 'song.wav')
+            self.wf = wave.open(file_path, 'rb')
+            self.p = pyaudio.PyAudio()
+            self.stream = self.p.open(
+                format = self.p.get_format_from_width(self.wf.getsampwidth()),
+                channels = self.wf.getnchannels(),
+                rate = self.wf.getframerate(),
+                output = True
+            )
         self.engine = tts.init()
         self.engine.setProperty('rate', 150)
         self.engine.setProperty('voice', 'english-us')
@@ -87,7 +149,7 @@ class AudioThread(threading.Thread):
     def run(self):
         while not self.stopped():
             logs = Log.objects.filter(reported=False)
-            if len(logs) == 0:
+            if len(logs) == 0 and self.play_music:
                 data = self.wf.readframes(self.CHUNK)
                 if len(data) == 0:
                     self.wf.rewind()
@@ -96,11 +158,11 @@ class AudioThread(threading.Thread):
             else:
                 log = logs.latest('id')
                 message = log.description
-                logs.update(reported=True)
                 # call(["say", "-r", "200", "-v", "Fred", str(message)])
                 self.engine.say(message)
                 # _ = self.engine.runAndWait()
                 self.engine.iterate()
+                Log.objects.filter(reported=False).update(reported=True)
         self.engine.endLoop()
         self.engine.stop()
         self.stream.close()
@@ -110,9 +172,9 @@ class AudioThread(threading.Thread):
 
 audio_thread = None
 
-def start_bluetooth_thread():
+def start_bluetooth_thread(play_music=True):
     global audio_thread
-    audio_thread = AudioThread()
+    audio_thread = AudioThread(play_music)
     audio_thread.daemon = True
     audio_thread.start()
     return
