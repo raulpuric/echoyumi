9fd7cb06319c1daf5b45d61db80507d23682d514
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2016-11-26 19:03:24 -0800

    adding gtts prerecord functionality

--- RobotThoughtApp/alexa.py
+++ RobotThoughtApp/alexa.py
@@ -1,11 +1,12 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 from django_alexa.api import fields, intent, ResponseBuilder
 from django.conf import settings
-import pyttsx as tts
+from gtts import gTTS
+# import pyttsx as tts
 import threading
 import pyaudio
 import wave
-import os
+import os, sys
 from subprocess import call
 
 from RobotThoughtApp.models import Log
@@ -125,23 +126,25 @@ def processAudio(play_music=True):
 
 
 
+####### Main Audio Thread #######
+
 class AudioThread(threading.Thread):
     def __init__(self, play_music=True):
         super(AudioThread, self).__init__()
         self._stop_event = threading.Event()
         self.play_music = play_music
 
-        if self.play_music:
-            self.CHUNK = 1024
-            file_path = os.path.join(settings.STATIC_ROOT, 'song.wav')
-            self.wf = wave.open(file_path, 'rb')
-            self.p = pyaudio.PyAudio()
-            self.stream = self.p.open(
-                format = self.p.get_format_from_width(self.wf.getsampwidth()),
-                channels = self.wf.getnchannels(),
-                rate = self.wf.getframerate(),
-                output = True
-            )
+        self.CHUNK = 1024
+        file_path = os.path.join(settings.STATIC_ROOT, 'song.wav')
+        self.wf = wave.open(file_path, 'rb')
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format = self.p.get_format_from_width(self.wf.getsampwidth()),
+            channels = self.wf.getnchannels(),
+            rate = self.wf.getframerate(),
+            output = True
+        )
+
         Log.objects.filter(reported=False).update(reported=True)
         self.engine = tts.init()
         self.engine.setProperty('rate', 150)
@@ -156,28 +159,75 @@ class AudioThread(threading.Thread):
     def stopped(self):
         return self._stop_event.isSet()
 
+    def get_music(self, chunk_size=None):
+        if chunk_size is None:
+            chunk_size = self.CHUNK
+        data = self.wf.readframes(chunk_size)
+        if len(data) < chunk_size:
+            self.wf.rewind()
+            data += self.wf.readframes(chunk_size - len(data))
+        return data
+
+    def merge_audio(data1, weight1, data2, weight2):
+        # http://stackoverflow.com/questions/4039158/mixing-two-audio-files-together-with-python
+        def bin_to_int(bin):
+            as_int = 0
+            for char in bin[::-1]: # iterate over each char in reverse (because little-endian)
+                as_int <<= 8
+                as_int += ord(char)
+            return as_int
+        def int_to_bin(as_int):
+            as_bin = ''
+            while as_int > 0:
+                as_bin += chr(as_int % 2**8)
+                as_int >>= 8
+            return as_bin
+
+        samples1 = [data1[i:i+2] for i in xrange(0, len(data1), 2)]
+        samples2 = [data2[i:i+2] for i in xrange(0, len(data2), 2)]
+
+        samples1 = [bin_to_int(s) for s in samples1]
+        samples2 = [bin_to_int(s) for s in samples2]
+
+        samples_avg = [int((s1*weight1 + s2*weight2)/(weight1 + weight2)) for (s1, s2) in zip(samples1, samples2)]
+
+        data = [int_to_bin(s) for s in samples_avg]
+        data = ''.join(data)
+
+        return data
+
+    def play_message(message):
+        file_path = os.path.join(settings.STATIC_ROOT, 'messages', message.lower().replace(" ", "_")+'.wav')
+        if os.path.isfile(file_path):
+            wf = wave.open(file_path, 'rb')
+            msg_data = wf.readframes(sys.maxint)
+            music_data = self.get_music(len(msg_data))
+            data = self.merge_audio(msg_data, 2.0, music_data, 1.0)
+            self.stream.write(data)
+        else:
+            self.engine.say(message)
+            self.engine.iterate()
+            # spawn thread to convert message to wav file
+            start_gtts_thread(message)
+        return
+
     def run(self):
         while not self.stopped():
             logs = Log.objects.filter(reported=False)
             if len(logs) == 0 and self.play_music:
-                data = self.wf.readframes(self.CHUNK)
-                if len(data) == 0:
-                    self.wf.rewind()
-                    data = self.wf.readframes(self.CHUNK)
+                data = self.get_music()
                 self.stream.write(data)
             else:
                 log = logs.latest('id')
                 message = log.description
-                # call(["say", "-r", "200", "-v", "Fred", str(message)])
-                self.engine.say(message)
-                # _ = self.engine.runAndWait()
-                self.engine.iterate()
+                self.play_message(message)
                 Log.objects.filter(reported=False).update(reported=True)
         self.engine.endLoop()
         self.engine.stop()
         self.stream.close()
         self.p.terminate()
         return
+
 
 
 audio_thread = None
@@ -195,3 +245,42 @@ def stop_bluetooth_thread():
         audio_thread.stop()
         audio_thread = None
     return
+
+
+
+####### gTTS Conversion Thread #######
+
+def gtts_to_wav(message):
+    tts = gTTS(text=message, lang='en')
+    file_name = message.lower().replace(" ", "_")
+    file_path = os.path.join(settings.STATIC_ROOT, 'messages', file_name)
+    # file_path = "static/messages/"+file_name
+    tts.save(file_path + ".mp3")
+    call(["ffmpeg", "-i", file_path + ".mp3",
+        "-acodec", "pcm_s16le",
+        "-ac", "1",
+        "-ar", "24000",
+        file_path + ".wav"])
+    os.remove(file_path + ".mp3")
+    return
+
+def audify_database():
+    database = [a.lower().replace(' ', '_') for a in Log.objects.values('description')]
+    database = set(database)
+    saved = [a[:-4] for a in os.listdir(os.path.join(settings.STATIC_ROOT, 'messages'))]
+    saved = set(saved)
+    
+    messages = database - saved
+    for message in messages:
+        gtts_to_wav(message)
+    return
+
+def start_gtts_thread(message):
+    gtts_thread = threading.Thread(target=gtts_to_wav, args=(message,))
+    gtts_thread.daemon = True
+    gtts_thread.start()
+    return
+
+import IPython
+IPython.embed()
+sys.exit(0)
