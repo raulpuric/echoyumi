1302c3d2a3a87fa65f5154d4f87e7b00770ff27d
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2016-12-08 14:03:02 -0800

    cleaning up file_path

--- robot_logger.py
+++ robot_logger.py
@@ -15,18 +15,21 @@ def log(message):
 
 	return
 
+
+file_path = "/home/autolab/Workspace/rishi_working/echoyumi/grasp_command.txt"
+
 """
 Get all the part names that were spoken to the Echo.
 """
 def getGraspCommands():
-	f = open("/home/autolab/Workspace/rishi_working/echoyumi/grasp_command.txt", 'r')
+	f = open(file_path, 'r')
 	line = f.read()
 	f.close()
 	if line is None or line == "":
 		return None
 	part_names = line.split(",")
 	# clearing file
-	open("/home/autolab/Workspace/rishi_working/echoyumi/grasp_command.txt", 'w').close()
+	open(file_path, 'w').close()
 	return part_names
 
 """
@@ -34,13 +37,13 @@ Get (without replacement) the first part name that was spoken to the Echo.
 Call this function iteratively to get the part names one at a time.
 """
 def getSingleGraspCommand():
-	f = open("/home/autolab/Workspace/rishi_working/echoyumi/grasp_command.txt", 'r')
+	f = open(file_path, 'r')
 	line = f.read()
 	f.close()
 	if line is None or line == "":
 		return None
 	part_names = line.split(",")
-	f = open("/home/autolab/Workspace/rishi_working/echoyumi/grasp_command.txt", 'w')
+	f = open(file_path, 'w')
 	f.write( ','.join(part_names[1:]) )
 	f.close()
 	return part_names[0]
